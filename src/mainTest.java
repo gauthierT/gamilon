9aec51815ad913584a100172e9e6d9682a00d246
Gauthier T. <gauthier.tuffou@gmail.com>
2016-06-02 16:46:54 +0200

    Remise à zéro du projet

---
+++ src/mainTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author NeoX_^
+ *
+ */
+public class mainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/tMain.java
+++
@@ -1,9 +0,0 @@
-
-public class tMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-// tst
-	}
-
-}
